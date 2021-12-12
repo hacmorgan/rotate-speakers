dbca2d37a3d6f16521199af41930978e06f2e2af
Hamish Morgan <ham430@gmail.com>
2021-12-13 00:51:52 +1100

    Generate aruco markers, need to print them out

---
+++ detect_aruco/generate_aruco.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+"""
+@file
+
+Generate aruco codes for the two speakers.
+
+@author Hamish Morgan
+@date   13/12/2021
+"""
+
+
+import cv2
+import numpy as np
+
+
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+
+left = np.zeros((300, 300, 1), dtype="uint8")
+right = left.copy()
+
+# print(f"Aruco dict: {aruco_dict}")
+
+cv2.aruco.drawMarker(dictionary=aruco_dict, id=0, sidePixels=300, img=left, borderBits=1)
+cv2.aruco.drawMarker(dictionary=aruco_dict, id=1, sidePixels=300, img=right, borderBits=1)
+
+cv2.imshow("left", left)
+cv2.imshow("right", right)
+cv2.waitKey(0)
